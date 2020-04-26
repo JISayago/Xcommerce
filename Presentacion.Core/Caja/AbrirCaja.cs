f18a3cc9c349ce876e753246c0ad0dc689435ef7
jnrdrgz <jnigrodriguez@gmail.com>
2020-04-26 15:28:59 -0300

    creados formularios caja abrir cerrar

---
+++ Presentacion.Core/Caja/AbrirCaja.cs
@@ -0,0 +1,30 @@
+﻿using Presentacion.Helpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using XCommerce.Servicios.Core.Caja;
+
+namespace Presentacion.Core.Caja
+{
+    public partial class AbrirCaja : Form
+    {
+        private readonly ICajaServicio _cajaServicio;
+        public AbrirCaja()
+        {
+            InitializeComponent();
+
+            _cajaServicio = new CajaServicio();
+
+            nombreUsuarioLbl.Text = DatosSistema.NombreUsuario;
+            
+        }
+
+
+    }
+}
