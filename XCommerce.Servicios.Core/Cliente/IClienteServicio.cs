9f4fe43b360c50b683548994b7b7e120bc05fc8e
MartinMiguelRR <martin97.rodri@gmail.com>
2020-05-03 02:02:36 -0300

    Agregado Cliente Servicio

---
+++ XCommerce.Servicios.Core/Cliente/IClienteServicio.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XCommerce.Servicios.Core.Cliente.DTO;
+
+namespace XCommerce.Servicios.Core.Cliente
+{
+    public interface IClienteServicio
+    {
+        IEnumerable<ClienteDTO> ObtenerCliente(string cadenaBuscar);
+
+        IEnumerable<ClienteDTO> ObtenerClienteEliminado(string cadenaBuscar);
+
+        ClienteDTO ObtenerClientePorId(long clienteId);
+
+        long Insertar(ClienteDTO clienteDto);
+
+        void Modificar(ClienteDTO clienteDto);
+
+        void Eliminar(long clienteId);
+    }
+}
