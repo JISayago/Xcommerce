cdf9e35252657e1e2743215bf89d6b7380d3531f
jnrdrgz <jnigrodriguez@gmail.com>
2020-05-28 17:23:18 -0300

    servicios de comprobante(debería ser factura)

---
+++ XCommerce.Servicios.Core/Comprobante/IComprobanteServicio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XCommerce.Servicios.Core.Comprobante.DTO;
+
+namespace XCommerce.Servicios.Core.Comprobante
+{
+    public interface IComprobanteServicio
+    {
+        long Generar(ComprobanteDTO dto);
+    }
+}
